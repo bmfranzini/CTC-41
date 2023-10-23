9704264fce00690af665a721c0d8a07b8d4fc0ec
RodrigoCardoso <rodrigo.cardosoo15@gmail.com>
2023-10-23 11:22:17 -0300

    finished fix details

---
+++ AnalisadorSintatico/build/CMakeFiles/runmylex.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runmylex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/runmylex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
